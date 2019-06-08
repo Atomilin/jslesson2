df132f5fbba9cf6753400e98b90cbc5a1badbb13
Atomilin <50828840+Atomilin@users.noreply.github.com>
2019-06-08 22:33:18 +0300

    homework_4

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function createDivWithText(text) {
    prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
  */
 function prepend(what, where) {
-    what.prepend(where);
+    where.insertBefore(what, where.firstChild);
 }
 
 /*
